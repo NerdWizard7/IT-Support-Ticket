ec8c3810ed323ddb312252581810e35d92d1c226
Caleb Smith <calebsmitty777@gmail.com>
2020-03-25 20:40:01 -0500

    Frames Converted to Panels

--- Main.py
+++ Main.py
@@ -21,6 +21,7 @@ class MainApp(wx.Frame):
 
         # Set up main panel
         self.mainMenuPanel = Panels.MainMenu(self)
+        self.mainMenuPanel.Hide()
         self.adminPanel = Panels.AdminLogin(self)
         self.adminPanel.Hide()
         self.clientPanel = Panels.ClientLogin(self)
@@ -32,7 +33,85 @@ class MainApp(wx.Frame):
         self.sizer.Add(self.clientPanel, 1, wx.EXPAND)
         self.SetSizer(self.sizer)
 
-        self.winStack = [self]
+        self.winStack = []
+
+        self.shownPanel = None
+        self.shownName = ''
+
+        self.pushWinStack(self.mainMenuPanel)
+
+# ---------------------Menu Bar--------------------------------
+    def loadMenuBar(self, shown):
+        if shown == "Client":
+            clientMenuBar = wx.MenuBar()
+
+            clientMenu3 = wx.Menu()
+            clientMenu3.Append(112, '&Preview')
+            clientMenu3.Append(114, '&Page Setup...')
+            clientMenu3.Append(115, '&Print...')
+            clientMenu3.AppendSeparator()
+            clientMenu3.Append(116, '&Refresh')
+            # clientMenu3.Append(117, '&Logout')
+            clientMenuBar.Append(clientMenu3, '&File')
+
+            clientMenu1 = wx.Menu()
+            clientMenu1.Append(105, '&Full Job Queue')
+            clientMenu1.Append(106, '&Completed Jobs')
+            clientMenuBar.Append(clientMenu1, '&View')
+
+            clientMenu2 = wx.Menu()
+            clientMenu2.Append(110, '&Database')
+            clientMenuBar.Append(clientMenu2, '&Settings')
+
+            self.Bind(wx.EVT_MENU, self.shownPanel.dataBaseSettings_OnClick, id=110)
+            self.Bind(wx.EVT_MENU, self.shownPanel.refreshButton_OnClick, id=116)
+            self.Bind(wx.EVT_MENU, self.shownPanel.viewFullQueue_OnClick, id=105)
+            self.Bind(wx.EVT_MENU, self.shownPanel.viewCompleted_OnClick, id=106)
+            self.Bind(wx.EVT_MENU, self.shownPanel.print_OnClick, id=115)
+            self.Bind(wx.EVT_MENU, self.shownPanel.pageSetup_OnClick, id=114)
+            self.Bind(wx.EVT_MENU, self.shownPanel.printPreview_OnClick, id=112)
+            self.Bind(wx.EVT_MENU, self.logout_OnClick, id=117)
+
+            self.SetMenuBar(clientMenuBar)
+        elif shown == "Admin":
+
+            adminMenuBar = wx.MenuBar()
+
+            adminMenu3 = wx.Menu()
+            adminMenu3.Append(100, 'Pre&view')
+            adminMenu3.Append(110, 'Page &Setup...')
+            adminMenu3.Append(120, '&Print...')
+            adminMenu3.Append(125, 'Add &Notes...')
+
+            adminMenu4 = wx.Menu()
+            adminMenu3.Append(126, "&Repor&ts", adminMenu4)
+
+            adminMenu3.AppendSeparator()
+            adminMenu3.Append(130, '&Refresh')
+            adminMenu3.Append(140, '&Logout')
+            adminMenuBar.Append(adminMenu3, '&File')
+
+            adminMenu1 = wx.Menu()
+            adminMenu1.Append(160, '&Completed Jobs')
+            adminMenuBar.Append(adminMenu1, '&View')
+
+            adminMenu2 = wx.Menu()
+            adminMenu2.Append(150, '&Database')
+            adminMenu2.Append(180, '&User Administraton')
+            adminMenuBar.Append(adminMenu2, '&Settings')
+            self.SetMenuBar(adminMenuBar)
+
+            # Bind Menu Items to Event Handlers
+            self.Bind(wx.EVT_MENU, self.shownPanel.pageSetup_OnClick, id=110)
+            self.Bind(wx.EVT_MENU, self.shownPanel.printPreview_OnClick, id=100)
+            self.Bind(wx.EVT_MENU, self.shownPanel.print_OnClick, id=120)
+            self.Bind(wx.EVT_MENU, self.shownPanel.refresh_OnClick, id=130)
+            self.Bind(wx.EVT_MENU, self.shownPanel.addNotes_OnClick, id=125)
+            self.Bind(wx.EVT_MENU, self.shownPanel.dataBaseSettings_OnClick, id=150)
+            self.Bind(wx.EVT_MENU, self.shownPanel.viewCompleted_OnClick, id=160)
+            self.Bind(wx.EVT_MENU, self.logout_OnClick, id=140)
+        else:
+            pass
 
 
     # Button Event Logic
@@ -40,13 +119,11 @@ class MainApp(wx.Frame):
         btn = evt.GetEventObject().GetLabel()  # Get the name of the button and store in btn variable
         id = evt.GetEventObject().GetId()  # Get the id of the button for differentiating between admin and client
         if btn == 'Admin':  # Admin Button
-            self.mainMenuPanel.Hide()
-            self.adminPanel.Show()
+            self.pushWinStack(self.adminPanel)
             self.SetTitle('Administrator Login')
             self.Layout()
         elif btn == 'Client':  # Client Button
-            self.mainMenuPanel.Hide()
-            self.clientPanel.Show()
+            self.pushWinStack(self.clientPanel)
             self.SetTitle('Client Login')
             self.Layout()
 
@@ -54,17 +131,19 @@ class MainApp(wx.Frame):
         id = evt.GetEventObject().GetId()
         if id == 10:
             print('Client')
+            panel = Panels.ClientPanel(self, u)
             valid = Credentials.passwordHasher(u, p)
             if valid == 0 or valid == 1:
-                self.pushWinStack(ClientFrame(self, u))
+                self.pushWinStack(panel)
             else:
                 msg = wx.MessageBox(valid, 'Login Notice')
 
         elif id == 20:
             print('Admin')
+            panel = Panels.AdminPanel(self, u)
             valid = Credentials.passwordHasher(u, p)
             if valid == 0:
-                self.pushWinStack(AdminFrame(self, u))
+                self.pushWinStack(panel)
             elif valid == 1:
                 msg = wx.MessageBox("Looks like you aren't an administrator. Contact IT if this is an error",
                                     'Not Authotized')
@@ -79,14 +158,25 @@ class MainApp(wx.Frame):
         win.Show(True)  # Show the menu
         win.SetFocus()
 
+    def logout_OnClick(self, evt):
+        self.popWinStack()
+
     def refreshWinStack(self):
-        self.winStack[len(self.winStack) - 1].Restore()
         for i in range(len(self.winStack)):
             if i != len(self.winStack) - 1:
-                self.winStack[i].Close()
+                self.winStack[i].Hide()
+        top = self.winStack[len(self.winStack) - 1]
+        top.Show()
 
-    def pushWinStack(self, newWindow):
-        self.winStack.append(newWindow)
+        self.shownPanel = top  # Get the current shown panel object
+        self.shownName = self.shownPanel.GetName()
+        print(self.shownName)
+        self.loadMenuBar(self.shownName)
+
+
+
+    def pushWinStack(self, newPanel):
+        self.winStack.append(newPanel)
         self.refreshWinStack()
 
     def popWinStack(self):
